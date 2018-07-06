455e580732c02353f7e6610d4151f63935942370
Ceo <36505595+merturuc@users.noreply.github.com>
2018-07-06 16:18:11 +0300

    Add files via upload

---
+++ webapi1/webapi1/Connections/Connection.cs
@@ -0,0 +1,58 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using webapi1.Model;
+
+namespace webapi1.Connections
+{
+
+    
+    public class Connection
+    {
+        private string ConnectString;
+        public Connection()
+        {
+            ConnectString =@"server=sql7.freemysqlhosting.net;userid=sql7245535;password=jCrl9m78yD;database=sql7245535;sslmode=none";
+        }
+
+        public List<User> UserList()
+        {
+            List<User> allUser = new List<User>();
+            using (MySqlConnection myConnectdb = new MySqlConnection(ConnectString)) {
+
+                using (MySqlCommand myCommanddb = myConnectdb.CreateCommand())
+                {
+                    myCommanddb.CommandText = "Select * from users";
+                    myCommanddb.CommandType = System.Data.CommandType.Text;
+                    myCommanddb.Connection = myConnectdb;
+                    myConnectdb.Open();
+
+                    using (MySqlDataReader myReader = myCommanddb.ExecuteReader())
+                    {
+                        while (myReader.Read())
+                        {
+                            allUser.Add(new User {Id =myReader.GetInt32(myReader.GetOrdinal("ID")),
+                                Company =myReader.GetString(myReader.GetOrdinal("Company")),
+                                Name = myReader.GetString(myReader.GetOrdinal("Name")),
+                                Surname = myReader.GetString(myReader.GetOrdinal("Surname")),
+                                Email = myReader.GetString(myReader.GetOrdinal("Email")),
+                                Officephone =myReader.GetInt32(myReader.GetOrdinal("OfficePhone")),
+                                Mobilephone = myReader.GetInt32(myReader.GetOrdinal("MobilePhone")),
+                                Faxadress =myReader.GetInt32(myReader.GetOrdinal("Fax")),
+                                Address =myReader.GetString(myReader.GetOrdinal("Address")),
+                                City =myReader.GetString(myReader.GetOrdinal("City")),
+                                Province =myReader.GetString(myReader.GetOrdinal("Province")),
+                                Country =myReader.GetString(myReader.GetOrdinal("Country")) });
+                        }
+                    }
+                }
+                myConnectdb.Close();
+            }
+            
+
+                return allUser;
+        }
+    }
+}
---
+++ webapi1/webapi1/Model/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi1.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public int Officephone { get; set; }
+        public int Mobilephone { get; set; }
+        public int Faxadress { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Country { get; set; }
+    }
+}
